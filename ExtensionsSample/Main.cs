88db387e5997a6f48f3a0a61b2da3d36abc6e038
Warren Moxley <wmoxley@yvr001osx.it1services.local>
2010-05-12 20:34:08 -0700

    Added MIT/X11 liscense.  Also utilized networkactivity indicator during google image search.

--- ExtensionsSample/Main.cs
+++ ExtensionsSample/Main.cs
@@ -1,3 +1,27 @@
+/*
+Copyright (c) 2010 Novell Inc.
+
+Author: Miguel de Icaza
+Updates: Warren Moxley
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+*/
 
 using System;
 using System.Collections.Generic;
@@ -21,17 +45,6 @@ namespace ExtensionsSample
 			UIApplication.Main (args);
 		}
 		
-		public bool NetworkIndicator
-		{
-			get
-			{
-				return UIApplication.SharedApplication.NetworkActivityIndicatorVisible;
-			}
-			set
-			{
-				UIApplication.SharedApplication.NetworkActivityIndicatorVisible = value;
-			}
-		}
 	}
 
 	// The name AppDelegate is referenced in the MainWindow.xib file.
@@ -58,7 +71,9 @@ namespace ExtensionsSample
 			
 			window.MakeKeyAndVisible ();
 			
+			Util.PushNetworkActive();
 			CreateSearch();
+			Util.PopNetworkActive();
 			
 			return true;
 		}
@@ -153,6 +168,8 @@ namespace ExtensionsSample
 
 		void CreateImages (string searchTerm)
 		{
+			Util.PushNetworkActive();
+			
 			ImageStore.ClearCache();
 
 			int i = 1;
@@ -172,6 +189,8 @@ namespace ExtensionsSample
 			DialogViewController dvc = new DialogViewController (root,true) { RotateUIEnabled = true };
 			
 			navigation.PushViewController (dvc, true);
+			
+			Util.PopNetworkActive();
 			
 		}
 	
